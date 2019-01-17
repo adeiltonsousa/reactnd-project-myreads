1610f63e58e7085065e056c1117c25edfaec6237
root <adeiltonsousaxavier@gmail.com>
2019-01-17 02:16:55 -0200

    Adição do React Router com rota do Search

--- src/components/SearchBook.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-class SearchBooks extends React.Component {
-
-
-}
-export default SearchBooks
---
+++ src/components/SearchBooks.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import '../App.css'
+
+const SearchBooks = props => {
+    return <div>Search Books</div>
+}
+
+export default SearchBooks
