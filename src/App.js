9ac3fbb4a679bd13d1831ed54950b925fce2117f
adeiltonsousa <adeiltonsousaxavier@gmail.com>
2019-01-17 18:21:42 -0200

    Metodo de consulta API e Modulo BookList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './components/SearchBooks'
+import BookList from './components/BookList'
 import { Route, Link } from 'react-router-dom'
 
-class BooksApp extends React.Component {
+
+class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-  }
+    books: []
+ }
+
+
+componentDidMount() {
+  BooksAPI.getAll().then((books) => this.setState({ books: books }));
+}
+  
+
 
   render() {
+    
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-            <div>Home Page
+            <div>
+              {console.table( this.state.books ) }
+              
+              <BookList books={ this.state.books }/>
+
               <Link
                   to='/search'
                   className='open-search'
@@ -28,7 +37,6 @@ class BooksApp extends React.Component {
         
         <Route exact path='/search' render={ SearchBooks }
             />
-
       </div>
     )
   }
